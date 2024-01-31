47f8e07732a76ef17049dae623b8ea6ffd5f609e
Sukrit <sukrit3007@outlook.com>
2024-01-31 21:48:36 +0530

    change 1

--- app/page.js
+++ app/page.js
@@ -8,49 +8,47 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   const [blogData, setBlogData] = useState([]);
 
-  const fetchBlog = async () =>{
+  const fetchBlog = async () => {
     try {
       const response = await axios.get('/api');
-      setBlogData(response.data.blogs) 
+      setBlogData(response.data.blogs);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   } 
 
-  const deleteBlog = async(id) =>{
+  const deleteBlog = async (id) => {
     try {
-      const response = await axios.delete('/api',{
-        params:{
-          mongoId:id
-        }
-      })
+      const response = await axios.delete('/api', {
+        params: {
+          mongoId: id,
+        },
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
     fetchBlog();
   }
 
-  useEffect(()=>{
-    fetchBlog(); 
-  },[])
+  useEffect(() => {
+    fetchBlog();
+  }, []);
 
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      {blogData.map((item,index)=>{
-        return(
-          <div>
-              <h1 key={index} className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                {item.title}
-              </h1>
-              <p key={index} className="leading-7 [&:not(:first-child)]:mt-6">
-                {item.content}
-              </p>
-              <Button variant="destructive" onClick={()=>deleteBlog(item._id)}>
-                Delete
-              </Button>
-          </div>
-        )
-      })}
+      {blogData.map((item) => (
+        <div key={item._id}>
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            {item.title}
+          </h1>
+          <p className="leading-7 [&:not(:first-child)]:mt-6">
+            {item.content}
+          </p>
+          <Button variant="destructive" onClick={() => deleteBlog(item._id)}>
+            Delete
+          </Button>
+        </div>
+      ))}
     </main>
   );
 }
