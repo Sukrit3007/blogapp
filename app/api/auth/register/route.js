04bc312efa637323026709e33daeb70bc5a74bfc
Sukrit <sukrit3007@outlook.com>
2024-02-01 22:01:13 +0530

    jwt token error commenting all

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,14 +1,14 @@
 // import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+// import bcrypt from "bcrypt";
 
 
 // const app = express.Router();
 
-export async function POST(request) {
-    const  {username,password} = request.body
+// export async function POST(request) {
+//     const  {username,password} = request.body
 
-    const blogs = await BlogModel.find({});
+//     const blogs = await BlogModel.find({});
 
-    return NextResponse.json({blogs:blogs})
-}
+//     return NextResponse.json({blogs:blogs})
+// }
