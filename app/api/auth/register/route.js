7e3ec90ae165775911be38a6ef2e0b8edf7a3388
Sukrit <sukrit3007@outlook.com>
2024-02-01 21:59:56 +0530

    publish new ui

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,14 @@
+// import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+
+// const app = express.Router();
+
+export async function POST(request) {
+    const  {username,password} = request.body
+
+    const blogs = await BlogModel.find({});
+
+    return NextResponse.json({blogs:blogs})
+}
