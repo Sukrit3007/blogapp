5d415a8cfd0cad5a53f95e9924bcd177c0571deb
Sukrit <sukrit3007@outlook.com>
2024-02-02 19:00:44 +0530

    fixed footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="flex flex-row gap-2 justify-center p-4 lg:px-24 z-50 border-t-4">
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
-        Built by <a href="https://www.linkedin.com/in/sukrit37/" style={{textDecoration: 'underline'}}>Sukrit</a>. The source code is available on <a href="" style={{textDecoration: 'underline'}}>Github</a>.
-        </p>
-      
+      <p className="leading-7 mt-6">
+        Built by <a href="https://www.linkedin.com/in/sukrit37/" className="underline">Sukrit</a>. The source code is available on <a href="https://github.com/your-github-repository" className="underline">Github</a>.
+      </p>
     </div>
   );
 };
